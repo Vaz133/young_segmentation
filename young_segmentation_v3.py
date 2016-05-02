51eadc7e900b357f72e3b0fd3b435ee1dd2408ef
Vahid Azimi <vahidazimi@MacBook-Pro.local>
2016-05-02 09:22:49 -0700

    most recent changes

--- young_segmentation_v3.py
+++ young_segmentation_v3.py
@@ -6,13 +6,20 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from matplotlib import image
 from sklearn.cluster import KMeans, MiniBatchKMeans
+from sklearn.preprocessing import scale, normalize
 from scipy.cluster import vq
-from scipy import ndimage
+from scipy import ndimage, misc
 from skimage.measure import label, regionprops
 import pandas as pd
 from collections import defaultdict
 import time
 import cPickle as pickle
+from math import pi
+from skimage.feature import peak_local_max, structure_tensor, structure_tensor_eigvals
+from skimage.morphology import watershed, disk, white_tophat, black_tophat, remove_small_objects
+from skimage.morphology import erosion, dilation, opening, skeletonize, closing, convex_hull_image
+from skimage.segmentation import mark_boundaries
+
 
 start_time = time.time()
 
@@ -25,75 +32,94 @@ parser.add_option("-o", "--output_directory", dest="output_dir", type="string", 
 class Segmentation:
 
 	def __init__(self, image, output_dir):
-		self.image = io.imread(image)
+
+		self.image = io.imread(image).astype(np.float64)
 		self.output_dir = output_dir
 		self.x = self.image.shape[0]
 		self.y = self.image.shape[1]
 		self.dimensions = self.image.shape[0] * self.image.shape[1]
+		# self.im_grey = io.imread(image, as_grey=True)
 		self.im_grey = ndimage.imread(image)[:,:,0]
 
 	def rgb2lab(self):
+		## convert rgb to lab space
 		lab = color.rgb2lab(self.image)
-		nuclei = lab[:,:,2]
+		lab1 = lab[:,:,0]
+		lab2 = lab[:,:,1]
+		lab3 = lab[:,:,2]
+		## extract lab intensity space corresponding to nuclei
+		# nuclei = lab3
+		nuclei = filters.gaussian(lab3, 1)
+		## flatten lab channels 
 		lab1 = np.reshape(lab[:,:,0], (1, self.dimensions))
-		print lab1
 		lab2 = np.reshape(lab[:,:,1], (1, self.dimensions))
 		lab3 = np.reshape(lab[:,:,2], (1, self.dimensions))
-		lab_flattened = np.vstack((vq.whiten(lab1),vq.whiten(lab2),vq.whiten(lab3)))
+		## stack lab channels into one array
+		lab_flattened = np.vstack((scale(lab1, axis=1),scale(lab2, axis=1),scale(lab3, axis=1)))
 		return nuclei, lab_flattened
 
 	def extract_gabor(self):
+
 		im_grey, lab_features = S.rgb2lab()
 		frequencies = [1.571]
-		orientation_rads = [0, 0.785398, 1.5708, 2.3562]
+		orientation_rads = [0, 0.785398, 1.5708, 2.35619]
+		## generate list of combinations
 		combos = list(IT.product(frequencies, orientation_rads))
 		list_of_mags = []
 		for item in combos:
 			real_gabor, imaginary_gabor = filters.gabor(im_grey, item[0], item[1])
-			real_gabor_flattened = np.reshape(real_gabor, (1, (real_gabor.shape[0]*real_gabor.shape[1])))
-			imaginary_gabor_flattened = np.reshape(imaginary_gabor, (1, (imaginary_gabor.shape[0]*imaginary_gabor.shape[1])))
-			mag = np.sqrt(np.square(real_gabor_flattened, dtype=np.float64)+np.square(imaginary_gabor_flattened, dtype=np.float64))
-			sigma = .5 * item[0]
+			## find the magnitude (square root of the sum of squares of real and imaginary gabor)
+			mag = np.sqrt(np.square(real_gabor, dtype=np.float64)+np.square(imaginary_gabor, dtype=np.float64))
+			sigma = .5 * ((2*pi)/item[0])
 			K = 2
-			mag_gaussian = vq.whiten(filters.gaussian(mag, K*sigma))
-			list_of_mags.append(mag_gaussian)
+			## apply gaussian filter and scale the result with zero mean and unit variance
+			mag_gaussian = filters.gaussian(mag, K*sigma)
+			mag_gaussian_flattened = np.reshape(mag_gaussian, (1, (self.x*self.y)))
+			list_of_mags.append(scale(mag_gaussian_flattened, axis=1))
 		list_of_mags = np.asarray(list_of_mags).reshape(len(combos),self.dimensions)
-		features = np.vstack((lab_features, list_of_mags))
+		## combine gabor features with lab features
+		im_grey_flattened = np.reshape(im_grey, (1, (self.x*self.y)))
+		features = np.vstack((im_grey_flattened, list_of_mags))
 		return features
 
 	def kmeans(self):
+		## performs KMeans on features, returns labels
 		features = S.extract_gabor().T
 		mkb = MiniBatchKMeans(n_clusters=3)
 		mkb.fit(features)
 		return mkb.labels_
 
 	def visualize(self):
+
 		labels = S.kmeans()
 		labels = labels.reshape(self.x, self.y)
 		intensity_list = []
 		final_list = []
-		final = self.image
+		final = self.im_grey
 		for i in range(3):
-			grey = self.im_grey.copy()
-			grey[labels != i] = 0
-			final_list.append(grey)
-			intensity_list.append(np.mean(grey))
+			## mask grayscale image with labels 0 through 2
+			im_grey = self.im_grey.copy()
+			im_grey[labels != i] = 0
+			## record mean intensities throughout labeled images
+			final_list.append(im_grey)
+			intensity_list.append(np.mean(im_grey))
+			plt.imsave("./testing"+str(i)+".png", im_grey)
 		min_index = intensity_list.index(min(intensity_list))
 		im = final_list[min_index]
-		im_grey = np.zeros((self.x, self.y))
-		im_grey[im != 0] = 1
-		plt.imsave("./testing1_mb.png", im_grey, cmap="gray")
+		## create and save binary mask
+		# im_grey = np.zeros((self.x, self.y))
+		# im_grey[im != 0] = 1
+		# plt.imsave("./testing1_mb.png", im_grey, cmap="gray")
+		## create and save grayscale mask
 		final[im == 0] = 0
-		plt.imsave("./testing1_color.png", final)
-		return im_grey
-
-	def save_label_matrix(self):
-		im = S.visualize()
-		labels = label(im)
-		pickle.dump(labels, open("./label_matrix.p", 'wb'))
+		labels, _ = ndimage.label(final)
+		final_removed = remove_small_objects(labels, min_size=30)
+		final[final_removed==0] = 0
+		# plt.imsave("./objsrm.png", final, cmap="gray")
+		return final
 
 	def labels(self):
-		img = S.visualize()
+		img, _ = S.visualize()
 		labels = label(img)
 		measurements = regionprops(labels, self.im_grey)
 		meas_dict = defaultdict(list)
@@ -135,15 +161,44 @@ class Segmentation:
 			meas_dict['weighted_moments_hu'].append(measurements[i]['weighted_moments_hu'])
 			meas_dict['weighted_moments_normalized'].append(measurements[i]['weighted_moments_normalized'])
 		meas_df = pd.DataFrame(meas_dict)
-		# meas_df.to_csv("./test.csv")
+		pickle.dump(meas_df, open("./feature_df.p", 'wb'))
+
+	def watershed(self, image):
+		im = S.visualize()
+		inv = abs(255 - im)
+		inv[inv == 255] = 0
+		selem = disk(3)
+		eroded = erosion(im, selem)
+		w_tophat = white_tophat(inv, selem)
+
+
+		fig, ax = plt.subplots(1, 3, figsize=(8, 4), sharex=True, sharey=True, subplot_kw={'adjustable': 'box-forced'})
+		ax1, ax2, ax3 = ax.ravel()
+		ax1.imshow(im, cmap="gray")
+		# ax1.set_title('seg')
+		ax2.imshow(inv, cmap="gray")
+		# ax2.set_title('original_ndimage')
+		ax3.imshow(w_tophat, cmap="gray")
+		# ax3.set_title('original_io')
+		# ax4.imshow(w_tophat, cmap="gray")
+
+		plt.show()
+
+
+		# plt.imsave("./watershed_386", a)
+
+
+
 
 (options, args) = parser.parse_args()
 
 S = Segmentation(options.image, options.output_dir)
 # S.rgb2lab()
+# S.extract_gabor()
+# S.kmeans()
 # S.visualize()
-S.save_label_matrix()
-# S.labels()
+# S.save_label_matrix()
+S.watershed(options.image)
 print time.time() - start_time
 
 
